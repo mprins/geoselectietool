01c9de630bde5c2593c886fb238b47a551422f18
Mark Prins <markprins@b3partners.nl>
2019-02-21 14:57:47 +0100

    https fixes

--- reverselocator.js
+++ reverselocator.js
@@ -1,5 +1,5 @@
 function reverseLocate(x,y,srs) {
-	var url = "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Locators/ESRI_Geocode_EU/GeocodeServer/reverseGeocode?location=%7By%3A"+y+"%2Cx%3A"+x+"%7D&distance=5000&outSR="+srs+"&f=json&callback=parseLocation";
+	var url = "https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Locators/ESRI_Geocode_EU/GeocodeServer/reverseGeocode?location=%7By%3A"+y+"%2Cx%3A"+x+"%7D&distance=5000&outSR="+srs+"&f=json&callback=parseLocation";
 	var script = document.createElement('script');
 	script.setAttribute('src', url);
 	document.getElementsByTagName('head')[0].appendChild(script);
